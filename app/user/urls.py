5f6ed60dcacf9c842ccf625a7c71043e953701c9
David-T7 <tesfayedawit524@gmail.com>
2024-06-07 15:01:16 +0300

    recipe api added

--- app/user/urls.py
+++ app/user/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 
 app_name = 'user'
 urlpatterns =[
-path('create/' , view=views.CreateUserView.as_view() , name='create'
-)
+path('create/' , view=views.CreateUserView.as_view() , name='create') ,
+path('token/' , view=views.CreateTokenView.as_view() , name='token'),
+path('me/' , view=views.ManageUserView.as_view() , name='me'),
+
 ]
